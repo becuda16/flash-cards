ab8235e3c86239b08dfe690070017c0700351bcf
becuda16 <rebeca.chirmicci.silva@escola.pr.gov.br>
2025-06-24 09:45:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    `Programação`
+    `Óque é Javascript?`,
+    `O javascript é uma linguagem de
+    progamação`
+)
+criaCartao(
+    `Geografia`,
+    `Qual é a capital do Paraná`,
+    `A capital do Paraná é curitiba` 
+)
+criaCartao(
+    `Programação`,
+    `O que é Python?`,
+    `O Python é uma linguagem de
+    programação`
+)
